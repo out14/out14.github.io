0b6353c27eb864f09227bf475da57e8e9a1db87d
gima <gima@naver.com>
2026-08-11 18:24:41 +0900

    json test

---
+++ package/script/list.js
@@ -0,0 +1,24 @@
+fetch('/package/data/data.json')
+    .then(res =>res.json())
+    .then(data =>{
+        const list = document.querySelector('.portfolioList');
+        data.forEach(element => {
+            const li = document.createElement('li');
+            li.innerHTML =`
+                <a href="#">
+                    <div class="portfolio-image">
+                        <img src="${element.image}" alt="${element.title}">
+                    </div>
+
+                    <div class="portfolio-info">
+                        <span>${element.category}</span>
+                        <h3>${element.title}</h3>
+                        <p>${element.description}</p>
+                    </div>
+                </a>
+            `
+            list.appendChild(li);
+        });
+
+
+})
